b10128868f71584703d7d76287fd9e32ad319653
Ram N. Thakur <ramthakur977@gmail.com>
2022-01-13 19:14:59 +0545

    This is the Second Commit

--- app/src/main/java/com/inim/canteenmealadmin/AddMenuFragment.java
+++ app/src/main/java/com/inim/canteenmealadmin/AddMenuFragment.java
@@ -17,35 +17,35 @@ import com.google.firebase.database.FirebaseDatabase;
 public class AddMenuFragment extends Fragment {
 
 
-    EditText foodIdET,foodNameET,foodPriceEt;
+    EditText foodIdET, foodNameET, foodPriceEt;
     Button menuBtn;
-    FirebaseDatabase fd=FirebaseDatabase.getInstance();
-    DatabaseReference ref =fd.getReference().child("menu");
+    FirebaseDatabase fd = FirebaseDatabase.getInstance();
+    DatabaseReference ref = fd.getReference().child("menu");
+
     public AddMenuFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_add_menu, container, false);
-        foodIdET=view.findViewById(R.id.foodIdET);
+        View view = inflater.inflate(R.layout.fragment_add_menu, container, false);
+        foodIdET = view.findViewById(R.id.foodIdET);
 
-        foodNameET=view.findViewById(R.id.foodNameET);
-        foodPriceEt=view.findViewById(R.id.foodPriceET);
-        menuBtn=view.findViewById(R.id.menuBtn);
+        foodNameET = view.findViewById(R.id.foodNameET);
+        foodPriceEt = view.findViewById(R.id.foodPriceET);
+        menuBtn = view.findViewById(R.id.menuBtn);
 
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fId=foodIdET.getText().toString();
-                String fName=foodNameET.getText().toString();
-                int fPrice=Integer.parseInt(foodPriceEt.getText().toString());
+                String fId = foodIdET.getText().toString();
+                String fName = foodNameET.getText().toString();
+                int fPrice = Integer.parseInt(foodPriceEt.getText().toString());
 
-                NewMenuDataHolder obj=new NewMenuDataHolder(fId,fName,fPrice);
+                NewMenuDataHolder obj = new NewMenuDataHolder(fId, fName, fPrice);
                 ref.child(fName).setValue(obj);
                 Toast.makeText(getContext(), "Update Complete", Toast.LENGTH_SHORT).show();
 
